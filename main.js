b0944cef2728f69c17fdf01dbf3c1c599e774e08
taborgreat <taborgreat@gmail.com>
2023-05-30 21:39:44 -0700

    Create main.js
    made canvas that has multidimensional for loop creating box depending on size range size

---
+++ main.js
@@ -0,0 +1,40 @@
+let canvas = document.getElementById("canvas");
+
+let sizeRange = document.getElementById("sizeRange");
+let pixelOutput = document.getElementById("output");
+let refreshButton = document.getElementById("refresh");
+
+
+
+
+sizeRange.oninput = () => pixelOutput.innerHTML = sizeRange.value;
+
+
+function createCanvas(size){
+    for(let i = 0; i < size; i++){
+        for(let j = 0; j < size; j++){
+            let pixel = document.createElement("div");
+            pixel.style.backgroundColor="white";
+            pixel.style.width = `${canvas.offsetWidth / size}px`;
+            pixel.style.height = `${canvas.offsetHeight / size}px`
+            pixel.id = "pixel";
+            pixel.addEventListener("mouseover", () => {pixel.style.backgroundColor="black"});
+            canvas.append(pixel);
+           
+        }
+    }
+}
+
+
+
+
+refreshButton.addEventListener("click", function () {
+    
+    while (canvas.firstChild) {
+            canvas.removeChild(canvas.firstChild);
+    }
+    
+    createCanvas(sizeRange.value);
+});
+
+
